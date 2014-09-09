b2adc8b48fca6c3f7d247fe7088f8dc8ef51567d
Neo Chae <mcchae7@gmail.com>
2014-09-08 21:52:23 -0700

    adding live & mock url for backend-less development. to be continue...

--- app/scripts/Neo.js
+++ app/scripts/Neo.js
@@ -22,6 +22,28 @@ var Neo = {
 		return newClass;
 	}
 };
+
 //namespace
 Neo.module = Neo.module || {};
 Neo.controller = Neo.controller || {};
+
+//e2e mock and live
+//if there is no mock url, then live will be used for mock.
+Neo.e2e = {
+	config: {
+		path: {
+			'Base':{
+				mock: './mock',
+				live: './api'
+			}
+		}
+	},
+	url: {
+		'SignIn': {
+			live: 'SignIn',//./api/SignIn, mock:./api/SignIn.json
+		},
+		'SignUp': {
+			live: 'SignUp'
+		}
+	}
+};
