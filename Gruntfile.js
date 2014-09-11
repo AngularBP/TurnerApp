9e87c70fb38bc45441b32ca4d3f0933d87aaf7d2
Neo Chae <mcchae7@gmail.com>
2014-09-11 11:03:42 -0700

    remove meaningless codes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,6 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  require('grunt-string-replace')(grunt);
-
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
